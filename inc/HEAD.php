1c9988c8560fd8cb0d8b7506d1665e31b445bf2b
AuTiMoThY <m62466@gmail.com>
2015-01-17 14:39:45 +0800

    START !!

---
+++ inc/HEAD.php
@@ -0,0 +1,24 @@
+<meta name="title" content="" />
+<meta name="keywords" content="">
+<meta name="description" content="" /><!-- 網站描述 -->
+<meta name="Copyright" content="" /><!-- 版權宣告 -->
+
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Don't forget to set your site up: http://google.com/webmasters -->
+<meta name="google-site-verification" content="" />
+
+<link rel="shortcut icon" href="favicon.ico" />
+
+<link rel="stylesheet" href="css/reset.css" />
+<link rel="stylesheet" href="css/ripples.css">
+<link rel="stylesheet" href="css/material-wfont.css">
+<link rel="stylesheet" href="css/font-awesome.css">
+<link rel="stylesheet" href="css/foundation.css" />
+<!-- <link rel="stylesheet" href="asset/fancybox/jquery.fancybox.css" /> -->
+<link rel="stylesheet" href="asset/FlexSlider/flexslider.css" />
+<!-- <link rel="stylesheet" href="css/effect2.css" /> -->
+<link rel="stylesheet" href="css/style.css" />
+
+<script src="js/libs/modernizr-2.7.1.dev.js"></script>
+<script src="js/ModernizrAu.js"></script>
---
+++ index.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<!--[if lt IE 7 ]> <html class="ie ie6 ie-lt10 ie-lt9 ie-lt8 ie-lt7 no-js" lang="zh"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie ie7 ie-lt10 ie-lt9 ie-lt8 no-js" lang="zh"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie ie8 ie-lt10 ie-lt9 no-js" lang="zh"> <![endif]-->
+<!--[if IE 9 ]>    <html class="ie ie9 ie-lt10 no-js" lang="zh"> <![endif]-->
+<!--[if gt IE 9]><!--><html class="no-js" lang="zh"><!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title></title>
+
+<?php
+// -------------------------------
+// SEO
+// CSS
+// Script in the HEAD
+// -------------------------------
+  include_once 'inc/HEAD.php';
+ ?>
+
+
+<?php
+//app
+  include_once 'inc/Social.php';
+ ?>
+
+<!--[if lt IE 9]>
+  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+</head>
+
+<body class="index_page">
+
+
+
+<?php
+// -------------------------------
+// Script in the FOOT
+// -------------------------------
+  include_once 'inc/ScriptFOOT.php';
+ ?>
+
+<?php
+// -------------------------------
+// google analytics
+// -------------------------------
+  include_once 'inc/ga.php';
+ ?>
+
+</body>
+</html>
